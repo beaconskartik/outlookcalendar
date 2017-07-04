8e4ab67e9eec0e018b56ee60c402fea2ef2c0731
ksachan <ksachan@adobe.com>
2017-07-04 19:59:44 +0530

    adding placeholder for agenda view

---
+++ outlook/calender/src/main/java/com/outlook/calender/agenda/OutlookAgendaView.java
@@ -0,0 +1,28 @@
+package com.outlook.calender.agenda;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by ksachan on 7/4/17.
+ */
+
+public class OutlookAgendaView extends RecyclerView
+{
+	public OutlookAgendaView(Context context)
+	{
+		super(context);
+	}
+	
+	public OutlookAgendaView(Context context, @Nullable AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+	
+	public OutlookAgendaView(Context context, @Nullable AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+	}
+}
