3d7341c0e8cedd78c44e2c849c10530e8674a032
ksachan <ksachan@adobe.com>
2017-07-07 14:58:23 +0530

    fixing crash due to improper cusrsor binding

--- outlook/calender/src/main/java/com/outlook/calender/agenda/OutlookAgendaCursorAdapter.java
+++ outlook/calender/src/main/java/com/outlook/calender/agenda/OutlookAgendaCursorAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
 
 import com.outlook.calender.OutlookCalendarEventQueryHandler;
+import com.outlook.calender.event.OutlookDayEventQueryHanlder;
 
 /**
  * Created by ksachan on 7/6/17.
@@ -16,7 +17,7 @@ public class OutlookAgendaCursorAdapter extends OutlookAgendaAdapter
 	public OutlookAgendaCursorAdapter(Context context)
 	{
 		super(context);
-		mHandler = new OutlookCalendarEventQueryHandler(context.getContentResolver(), this);
+		mHandler = new OutlookDayEventQueryHanlder(context.getContentResolver(), this);
 	}
 	
 	@Override
@@ -28,26 +29,8 @@ public class OutlookAgendaCursorAdapter extends OutlookAgendaAdapter
 	@Override
 	protected void loadEvents(long timeMillis)
 	{
-		mHandler.startQuery(0, timeMillis, CalendarContract.Events.CONTENT_URI, EVENTS_PROJECTION,
-				"("
-				+ CalendarContract.Events.DTSTART + ">=? AND "
-				+ CalendarContract.Events.DTSTART + "<?) OR ("
-				+ CalendarContract.Events.DTSTART + "<? AND "
-				+ CalendarContract.Events.DTEND + ">=?) AND "
-				+ CalendarContract.Events.DELETED + "=?",
-				new String[]{String.valueOf(timeMillis), String.valueOf(timeMillis + DateUtils.DAY_IN_MILLIS)
-						 , String.valueOf(
-						timeMillis), String.valueOf(timeMillis + DateUtils.DAY_IN_MILLIS), "0"},
-				CalendarContract.Events.DTSTART + " ASC");
+		mHandler.startQuery(timeMillis, timeMillis, timeMillis + DateUtils.DAY_IN_MILLIS);
 	}
 	
-	private static final String[] EVENTS_PROJECTION = new String[]{
-			CalendarContract.Events._ID,
-			CalendarContract.Events.CALENDAR_ID,
-			CalendarContract.Events.DTSTART,
-			CalendarContract.Events.DTEND,
-			CalendarContract.Events.TITLE
-	};
-	
-	private final OutlookCalendarEventQueryHandler mHandler;
+	private final OutlookDayEventQueryHanlder mHandler;
 }
